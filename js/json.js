5f60f3150872cb07c5fd62d5205d4423ea598da8
Manuel Alejandro Daza <manuel.daza90@gmail.com>
2016-07-14 15:18:56 -0500

    primer commit
    apertura del proyecto para NextU del modulo 1

---
+++ js/json.js
@@ -0,0 +1,54 @@
+{
+	"estudiantes":[
+		{
+			"codigo": "001",
+			"nombre": "Andrés Narvaez",
+			"nota": "2.0"
+		},
+		{
+			"codigo": "002",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+		{
+			"codigo": "003",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+		{
+			"codigo": "004",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+		{
+			"codigo": "005",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+		{
+			"codigo": "006",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+		{
+			"codigo": "007",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+		{
+			"codigo": "008",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+		{
+			"codigo": "009",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+		{
+			"codigo": "010",
+			"nombre": "Manuel",
+			"nota": "4.8"
+		},
+	]
+}
